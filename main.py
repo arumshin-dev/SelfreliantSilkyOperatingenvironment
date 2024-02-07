b99dc1b6b9db5a2264505fb8788ff910a48cfaa8
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-07 16:17:17 +0000

    while

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from random import randint
-user_choice=int(input("Chooose a number between 1 and 10: "))
+print("Welcome to the Number Guessing Game!")
 pc_choice=randint(1,10)
-if user_choice==pc_choice:
-  print("You won!")
-elif user_choice>pc_choice:
-  print("Lower! Correct number was", pc_choice)
-elif user_choice<pc_choice:
-  print("Higher! Correct number was", pc_choice)
+playing=True
+while playing:
+  user_choice=int(input("Chooose a number between 1 and 10: "))
+  if user_choice==pc_choice:
+    print("You won!")
+    playing=False
+  elif user_choice>pc_choice:
+    print("Lower!")
+  elif user_choice<pc_choice:
+    print("Higher!")
+
+print("Game over! Thank you for playing!")
