0f0cc3d7c8c0fb9490f0a621c500acb8e5d483ac
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-07 15:06:22 +0000

    return

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ def pay_tax(tax):
   print("thank you for paying",tax)
 
 to_pay = tax_calc(1500000000)
-pay_tax(to_pay)
+#pay_tax(to_pay)
+#pay_tax(tax_calc(1800000))
+
+def make_juice(fruit):
+  return f"🍹{fruit}+🥤"
+
+def add_ice(juice):
+  return f"🧊{juice}+🧊"
+
+def add_sugar(iced_juice):  
+  return f"🍬{iced_juice}+🍬" 
+juice = make_juice("🍎")
+cold_juice = add_ice(juice)  
+perfect_juice = add_sugar(cold_juice)
+print(perfect_juice)
