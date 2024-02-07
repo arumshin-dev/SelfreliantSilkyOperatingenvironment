ca539e5a7793edd069979be4e04eb9f989eb22b4
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-07 16:38:15 +0000

    methods

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-from random import randint
-print("Welcome to the Number Guessing Game!")
-pc_choice=randint(1,10)
-playing=True
-while playing:
-  user_choice=int(input("Chooose a number between 1 and 10: "))
-  if user_choice==pc_choice:
-    print("You won!")
-    playing=False
-  elif user_choice>pc_choice:
-    print("Lower!")
-  elif user_choice<pc_choice:
-    print("Higher!")
-
-print("Game over! Thank you for playing!")
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+print(days_of_week.pop())
+print(days_of_week.count("Wednesday"))
