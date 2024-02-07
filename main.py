86598a00fe02ad58c645bbf023d1230a0158adc3
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-07 14:52:11 +0000

    return values

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ def plus(a=0,b=0):
   print(f"{a} + {b} =",a+b)
   print(f"{a} + {b} = {a+b}")
 
-plus()
-plus(1,2)
+#plus()
+#plus(1,2)
+
+def tax_calc(money):
+  return money * 0.35
+
+def pay_tax(tax):
+  print("thank you for paying",tax)
+
+to_pay = tax_calc(1500000000)
+pay_tax(to_pay)
