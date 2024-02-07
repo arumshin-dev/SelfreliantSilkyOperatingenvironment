b54d2a76e938707e83ddcfd48934ecab7e772718
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-07 16:01:34 +0000

    random

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-age=int(input("How old are you?"))
-print("You are",age,"years old")
-print(type(age))
-if age<10:
-  print("You are a child")
-elif age<20:
-  print("You are a teenager")
-elif age>=30 and age<=50:
-  print("You are an adult")
-elif age==60 or age==70:
-  print("You are old")
-else:
-  print("You are a senior")
+from random import randint
+user_choice=int(input("Chooose a number between 1 and 10: "))
+pc_choice=randint(1,10)
+if user_choice==pc_choice:
+  print("You won!")
+elif user_choice>pc_choice:
+  print("Lower! Correct number was", pc_choice)
+elif user_choice<pc_choice:
+  print("Higher! Correct number was", pc_choice)
