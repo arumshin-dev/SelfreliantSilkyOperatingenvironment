3eacd83760a811a157372160b2ae18bfa348e262
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-07 16:51:13 +0000

    tuples

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print(days_of_week)
 print(days_of_week.pop())
 days_of_week.append("Sunday")
 print(days_of_week)
+
+days=("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+print(days)
+print(days[-2])
