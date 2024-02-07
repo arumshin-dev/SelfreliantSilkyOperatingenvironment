74eb5fd1cc6918a263c0a845ce2a4b2ac9ae2833
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-07 15:50:24 +0000

    input and or

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-winner=10
-
-if winner>10:
-  print("Winner is greater than 10")
-elif winner<10:
-  print("Winner is less than 10")
+age=int(input("How old are you?"))
+print("You are",age,"years old")
+print(type(age))
+if age<10:
+  print("You are a child")
+elif age<20:
+  print("You are a teenager")
+elif age>=30 and age<=50:
+  print("You are an adult")
+elif age==60 or age==70:
+  print("You are old")
 else:
-  print("Winner is 10")
-  
+  print("You are a senior")
