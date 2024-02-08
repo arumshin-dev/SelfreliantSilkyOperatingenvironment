245ea32c83d5d5b9efc9de79f9886a9ef54cfb31
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-08 14:49:14 +0000

    url formatting -status code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from requests import get
+
 websites = (
   "google.com",
   "airbnb.com",
@@ -6,8 +8,17 @@ websites = (
   "https://tiktok.com"
 )
 
+results = {}
+
 for website in websites:
   if not website.startswith("https://"):
     website = f"https://{website}"
-  print(website)
+  response=get(website)
+  if response.status_code == 200:
+    results[website] = "OK"
+  else:
+    results[website] = "FAILED"
+
+print(results)
+  
   
