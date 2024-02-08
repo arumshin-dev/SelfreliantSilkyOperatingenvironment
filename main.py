84222f9738222d853b23b98a6fd3eb2e6be5f11c
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-08 15:50:23 +0000

    class methods

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-from requests import get
+class Puppy:
+  
+  def __init__(self, name, color):
+    self.name = name
+    self.age = 0.1
+    self.color = color
+    
+  def __str__(self):
+    return f"Puppy name: {self.name}, age: {self.age}, color: {self.color}"
+    
+  def woof_woof(self):
+    print(f"{self.name} woof woof!")
 
-websites = (
-  "google.com",
-  "airbnb.com",
-  "https://twitter.com",
-  "facebook.com",
-  "https://tiktok.com"
+  def introduce(self):
+    self.woof_woof()
+    print(f"Hi, my name is {self.name} and I'm a {self.color} puppy.")
+    
+ruffus = Puppy(
+  name="Ruffus", 
+  color="brown",
+)
+bibi = Puppy(
+  name="Bibi", 
+  color="white",
 )
 
-results = {}
+print(ruffus, bibi)
+ruffus.introduce()
+bibi.introduce()
 
-for website in websites:
-  if not website.startswith("https://"):
-    website = f"https://{website}"
-  response=get(website)
-  if response.status_code == 200:
-    results[website] = "OK"
-  else:
-    results[website] = "FAILED"
-
-print(results)
-  
-  
