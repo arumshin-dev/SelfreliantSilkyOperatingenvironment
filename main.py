2cfc96b25250ec610dcbcb25bad6ec66dec3a10f
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-29 10:32:35 +0000

    wanted site playwright

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import sync_playwright
+import time
+from bs4 import BeautifulSoup
 
 #동기화 방식으로 사용을 시작합니다.
 play = sync_playwright().start()
@@ -14,12 +16,38 @@ browser = play.chromium.launch(headless=False)
 page = browser.new_page()
 
 #아래 주소로 이동합니다.
-#page.goto("https://mandloh.tistory.com/manage/posts/")
-page.goto("https://google.com")
+page.goto("https://www.wanted.co.kr/")
 
 #페이지의 screenshot을 저장합니다.
-page.screenshot(path="screenshot.png")
+#page.screenshot(path="screenshot.png")
+
+time.sleep(3)  #3초 기다림
+
+page.click("button.Aside_searchButton__Xhqq3")  #버튼 클래스 선택 클릭
+#page.locator("button.Aside_searchButton__Xhqq3").click()#버튼 클릭 동일
+
+time.sleep(3)
+
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+
+time.sleep(3)
+
+page.keyboard.down("Enter")  #엔터키를 누른다
+
+time.sleep(5)
+
+page.click("a#search_tab_position")
+
+for x in range(5):
+  time.sleep(5)
+
+  page.keyboard.down("End")
+
+time.sleep(5)
+
+content = page.content()
 
 #playwright를 종료합니다.
-#playwright.stop()
-#play.stop()
+play.stop()  #메모리 누수 방지 위하여 꺼주기
+
+soup = BeautifulSoup(content, "html.parser")
