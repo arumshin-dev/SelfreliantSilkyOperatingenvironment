c09cbeac713edf2b9ab3125f673d551b7cff590d
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-03-03 12:14:44 +0000

    Flask주석

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
+#Flask 객체 생성
 app = Flask("JobScrapper")
 
+#decorator 루트 라우팅
 @app.route("/")
+#function 루트 함수
 def home():
+  #return "Hello World"
   return "hey there!"
 
+#ip주소를 0.0.0.0으로 설정해줌 서버실행
 app.run("0.0.0.0")
