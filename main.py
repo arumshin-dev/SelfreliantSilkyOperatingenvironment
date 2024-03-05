34f3ff88d89dc862c2c839500d951dda516e58d4
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-03-05 09:53:02 +0000

    cache

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from extractors.wwr import extract_wwr_jobs
 #Flask 객체 생성
 app = Flask("JobScrapper")
 
+#cache 저장 fake db
+db = {}
+
 #decorator 루트 라우팅
 @app.route("/")
 #function 루트 함수
@@ -16,11 +19,18 @@ def home():
 @app.route("/search")
 def search():
   # print(request.args)
+  print(db)
   keyword = request.args.get("keyword")
-  # indeed = extract_indeed_jobs(keyword)
-  wwr = extract_wwr_jobs(keyword)
-  # jobs = indeed + wwr
-  jobs = wwr
+  if keyword in db:
+    print("이미 검색한거")
+    jobs = db[keyword]
+  else:
+    print("처음 검색")
+    # indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    # jobs = indeed + wwr
+    jobs = wwr
+    db[keyword] = jobs
   return render_template("search.html", keyword=keyword, jobs=jobs)
 
 #ip주소를 0.0.0.0으로 설정해줌 서버실행
